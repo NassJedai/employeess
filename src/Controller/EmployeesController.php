f376177b5287c2fc05f1776524761e9b33eccdfe
Nassjedai <jedainassim@gmail.com>
2023-01-14 16:53:48 +0100

    formulaire employee instalé

--- src/Controller/EmployeesController.php
+++ src/Controller/EmployeesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Employees;
+use App\Form\EmployeeType;
 use App\Repository\EmployeesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,6 +19,19 @@ class EmployeesController extends AbstractController
         //dd($employees);
         return $this->render('employees/index.html.twig', [
             'employees' => $employees,
+        ]);
+    }
+
+
+    #[Route('/employee/nouveau', name:'app_employees_nouveau', methods: ['GET', 'POST'])]
+    public function new(): Response
+    {
+        $employee = new Employees();
+        $form = $this->createForm(EmployeeType::class, $employee);
+
+        return $this->render('employees/new.html.twig', [
+            'form' => $form->createView()
+
         ]);
     }
 }
---
+++ src/Form/EmployeeType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employees;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EmployeeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('birthDate')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('gender')
+            ->add('hireDate')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Employees::class,
+        ]);
+    }
+}
